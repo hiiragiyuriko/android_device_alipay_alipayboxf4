1a1228b0f3dec414e1686c6aa8175433764661fc
Hiiragi <hiiragiyuriko@gmail.com>
2021-05-30 20:35:28 +0800

    device.mk: include overlay directory

--- device.mk
+++ device.mk
@@ -1,2 +1,6 @@
 $(call inherit-product, vendor/alipay/alipayboxf4/alipaybox-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
