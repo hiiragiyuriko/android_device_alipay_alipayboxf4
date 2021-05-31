e693cb33fb88574c7f7f43b20aac77306d0068e9
Hiiragi <hiiragiyuriko@gmail.com>
2021-05-31 10:58:57 +0800

    AndroidProduct.mk: add COMMON_LUNCH_CHOICES

--- AndroidProduct.mk
+++ AndroidProduct.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_alipayboxf4.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_alipayboxf4-user \
+    lineage_alipayboxf4-userdebug \
+    lineage_alipayboxf4-eng
